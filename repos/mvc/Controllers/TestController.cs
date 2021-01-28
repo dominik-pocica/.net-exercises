931b0c83004eb9fa581f2bc336dcbde65ebad0dd
dominik-pocica <56959391+dominik-pocica@users.noreply.github.com>
2021-01-28 10:32:05 +0100

    v1

---
+++ repos/mvc/Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ITestRepository repository;
+
+        public TestController(ITestRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string StringValue()
+        {
+            return "Hello World!";
+        }
+        public IActionResult RedirectToGoogle()
+        {
+            return Redirect("http://www.google.com/");
+        }
+        public IActionResult GetJson()
+        {
+            return Json(new { Name = "Jan", Surname = "Kowalski" });
+        }
+        public IActionResult TestModel()
+        {
+            var model = repository.GetItems();
+            return View(model);
+        }
+    }
+}
---
+++ repos/mvc/Models/TestRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.Models
+{
+    public class TestRepository : ITestRepository
+    {
+        public IEnumerable<TestModel> GetItems()
+        {
+            return new List<TestModel>
+            {
+                new TestModel { TestMessage = "marchew" },
+                new TestModel { TestMessage = "Wiadomosc z modelu 2" },
+                new TestModel { TestMessage = "Wiadomosc z modelu 3" },
+                new TestModel { TestMessage = "Wiadomosc z modelu 4" },
+            };
+        }
+    }
+}
